35e810e3e6efafaade8129f29536358fad8c6134
jdifek <givotkov11@gmail.com>
2025-08-20 18:38:52 +0200

    wqwqwq

--- routes/proxyRoutes.js
+++ routes/proxyRoutes.js
@@ -81,4 +81,62 @@ router.get('/', authMiddleware, proxyController.getProxies);
  */
 router.patch('/:id/link', authMiddleware, proxyController.linkProxy);
 
+/**
+ * @swagger
+ * /proxies/{id}:
+ *   put:
+ *     summary: Update proxy data
+ *     tags: [Proxies]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               ip:
+ *                 type: string
+ *               port:
+ *                 type: integer
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               status:
+ *                 type: string
+ *                 enum: [ALIVE, DEAD]
+ *     responses:
+ *       200:
+ *         description: Proxy updated successfully
+ */
+router.put('/:id', authMiddleware, proxyController.updateProxy);
+
+/**
+ * @swagger
+ * /proxies/{id}:
+ *   delete:
+ *     summary: Delete proxy
+ *     tags: [Proxies]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Proxy deleted successfully
+ */
+router.delete('/:id', authMiddleware, proxyController.deleteProxy);
+
 module.exports = router;
